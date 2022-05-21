d7fc3e585e2534f13770b012402dddb014670655
treblig-punisher <gilbertplayx@hotmail.com>
2022-05-21 15:51:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,46 @@ const progressBar = document.querySelector(".progress-bar");
 const knob = document.querySelector(".knob");
 const moneyAmount = document.querySelector(".money-amount");
 let pseudoElementWidth = 16;
+let knobDropped = true;
+let mouseEnteredKnob = false;
+let storedMouseCoordinates={
+    x: 0,
+    y: 0,
+    xPrevious: 0,
+    yPrevious: 0,
 
-function knobDragging(){
-    const progressBarWidth = progressBar.style.width();
-    clampValue()
+}
+window.addEventListener('mousemove', updateMousePosition)
+
+knob.addEventListener('onmouseenter', mouseEntered);
+knob.addEventListener('onmouseup', mouseLeftKnob);
+knob.addEventListener('onmousedown', mouseIsDown);
+
+function updateMousePosition(e)
+{
+    if(!knobDropped)
+    {
+        storedMouseCoordinates.x = e.X
+        console.log(storedMouseCoordinates.x);
+    }
+}
+
+function mouseIsDown()
+{
+    if(!knobDropped)
+    {
+
+    }
+}
+
+function mouseEntered()
+{
+    mouseEnteredKnob = true;
+}
+
+function mouseLeftKnob()
+{
+    mouseEnteredKnob = false;
 }
 
 function clampValue(value, max, min)
